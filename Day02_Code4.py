59eee8b2934e102836d83aaa195a19fc0c9b9a7d
AVINASH <avinashyellpragada80@gmail.com>
2026-08-13 12:45:29 +0530

    add

---
+++ Day02_Code4.py
@@ -0,0 +1,9 @@
+while True:
+    numb=int(input("Enter a number:"))
+    if numb<=50:
+      for i in range(1,numb+1):
+       if i%2==1:
+        print(i)
+      break
+    else:
+        print("Please enter number below 50")
